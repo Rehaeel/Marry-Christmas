32952877a155a3ebca848c8fb80161c1a12f32db
Rafal Prokopiak <rafal@prokopiak.pl>
2021-12-11 14:48:57 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Santa from "./photos/santa.png";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [santaVisibility, setSantaVisibility] = useState(false);
+    const [wiggle, setWiggle] = useState(false);
+    const showSanta = () => {
+        setSantaVisibility(!santaVisibility);
+
+        setTimeout(() => {
+            setWiggle(true);
+        }, 1000);
+
+        setTimeout(() => {
+            setWiggle(false);
+            setSantaVisibility(false);
+        }, 2000);
+    };
+
+    return (
+        <>
+            <div className="bg-image"></div>
+            <button onClick={showSanta}>Marry Christmas!</button>
+            <img
+                id="santa"
+                className={`${santaVisibility ? "" : "hidden"} ${
+                    wiggle ? "wiggle" : ""
+                }`}
+                src={Santa}
+                alt="Santa Claus"
+            />
+        </>
+    );
 }
 
 export default App;
